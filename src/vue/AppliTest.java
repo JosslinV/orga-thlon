c521a96ddf7fccc28df54c7bcdf1bf920ddd42a5
Josslin VARON <josslinvaron@gmail.com>
2018-03-05 13:55:35 +0100

    Reformate le code de la classe de test des vue

--- src/vue/AppliTest.java
+++ src/vue/AppliTest.java
@@ -1,12 +1,11 @@
 package vue;
 
 public class AppliTest {
-	
-  public static void main (String []a){
-    new FenetreNouvelleSousTache();
-    new FenetreNouveauMateriel();
-    new FenetreNouvelleCourse();
-   }
-  
- }
 
+	public static void main(String[] a) {
+		new FenetreNouvelleSousTache();
+		new FenetreNouveauMateriel();
+		new FenetreNouvelleCourse();
+	}
+
+}
