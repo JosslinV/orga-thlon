4fd4f00882ee00b1c0a50b5e7f494962429546a4
Josslin VARON <josslinvaron@gmail.com>
2018-03-05 13:55:04 +0100

    Ajoute le modele course

---
+++ src/modele/Course.java
@@ -0,0 +1,14 @@
+package modele;
+
+public class Course {
+
+	private String nom;
+	private String format;
+	/* private classe à définir dateC; */
+	private String descriptionCourse;
+	private String lieu;
+	private int nbParticipants;
+	private String parcours;
+	
+	
+}
