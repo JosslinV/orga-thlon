a11614e451a5bea12136f45b266438596a371edf
Josslin VARON <josslinvaron@gmail.com>
2018-03-05 13:54:47 +0100

    Réorganise les fichiers du projet

--- src/vue/AppliTest.java
+++ src/vue/AppliTest.java
@@ -1,9 +1,12 @@
 package vue;
+
 public class AppliTest {
+	
   public static void main (String []a){
     new FenetreNouvelleSousTache();
     new FenetreNouveauMateriel();
     new FenetreNouvelleCourse();
    }
+  
  }
 
