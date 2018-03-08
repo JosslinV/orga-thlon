5d3b3216db969ab087c9a275605c7b638701c535
Josslin VARON <josslinvaron@gmail.com>
2018-03-08 12:29:53 +0100

    Ajoute la sortie "bonjour"

--- src/modele/Main.java
+++ src/modele/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         MySQLAccess dao = new MySQLAccess();
         dao.readDataBase();
+        System.out.print("bonjour");
     }
 
 }
