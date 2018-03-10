43d9366974478d153166ef7d2110723cc2a64d24
Gege <Gege@Gege-PC.home>
2018-03-10 15:26:25 +0100

    constructerur et getter JDBC - Retrait des ambiguités des attributs

--- src/modele/Course.java
+++ src/modele/Course.java
@@ -1,37 +1,106 @@
 package modele;
 
+
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.Scanner ;
+
 
 public class Course {
 	
-	private String nom;
+	private String nom_course;
 	private String format;
-	private Date dateC;
-	private String descriptionCourse;
-	private String lieu;
+	private Date date_course;
+	private String description_course;
+	private String lieu_course;
 	private int nbParticipants;
 	private String parcours;
-	private String commentaire;
+	private String commentaire_course;
 	private float budget;
 	private String nom_ville;
 	private int cp_ville;
 	private String type_epreuve;
 	private int duree;
 	private boolean agrement;
-	private boolean predefini;
+	private int predefini;
 	
-	public Course(String nom, Date dateC, boolean predefini) {
-		this.nom = nom;
-		this.dateC = dateC;
-		this.predefini = predefini;
+	private Connection connect = null;
+    private Statement statement = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
+	
+	
+	
+	
+	public Course (String nom1, int predefini) throws Exception {
+		
+		
+		 try {
+		Class.forName("com.mysql.jdbc.Driver");
+		connect = DriverManager.getConnection("jdbc:mysql://localhost/testorga?" + "user=sqluser&password=orgathlon");
+		  preparedStatement = connect.prepareStatement("insert into  testorga.course values (default, ?, ?, ?, ? , ?, ?,?,?,?,?,?,?)");
+          preparedStatement.setString(1, nom1);
+          preparedStatement.setString(2, "");
+          preparedStatement.setDate(3, new java.sql.Date(1900, 01, 01));
+          preparedStatement.setString (4, "");
+          preparedStatement.setString(5, "");
+          preparedStatement.setInt(6, predefini);
+          preparedStatement.setDouble(7, 0);
+          preparedStatement.setInt(8, 3);
+          preparedStatement.setString(9, "");
+          preparedStatement.setString(10, "");
+          preparedStatement.setInt(11, 10000);
+          preparedStatement.setString(12, "");
+          preparedStatement.executeUpdate();
+          
+		 } catch (Exception e) {
+	            throw e;
+	        } finally {
+	            close();
+	        }
 	}
 	
-	public String getNom() {
+	public String getNom(int id) throws Exception {
+		//Au lieu de retourner le nom, retourne un select du nom pour une course donnée 
+		
+		try {
+		Class.forName("com.mysql.jdbc.Driver");
+		connect = DriverManager.getConnection("jdbc:mysql://localhost/testorga?" + "user=sqluser&password=orgathlon");
+		statement = connect.createStatement();
+		//resultSet = statement.executeQuery("select * from testorga.course");
+       
+         
+		preparedStatement = connect.prepareStatement("SELECT nom_course from testorga.course WHERE id_Course = "+id);
+        resultSet = preparedStatement.executeQuery();
+        
+        String nom="" ;
+        
+      while(resultSet.next()) {
+    	  
+       nom = nom + resultSet.getString("nom_course");
+       
+      }
 		return nom;
+		
+        
+        
+		
+	} catch (Exception e) {
+        throw e;
+    } finally {
+        close();
+    }
+
 	}
 
 	public void setNom(String nom) {
-		this.nom = nom;
+		this.nom_course = nom;
 	}
 
 	public String getFormat() {
@@ -43,27 +112,27 @@ public class Course {
 	}
 	
 	public Date getDateC() {
-		return dateC;
+		return date_course;
 	}
 
 	public void setDateC(Date dateC) {
-		this.dateC = dateC;
+		this.date_course = dateC;
 	}
 
 	public String getDescriptionCourse() {
-		return descriptionCourse;
+		return description_course;
 	}
 
 	public void setDescriptionCourse(String descriptionCourse) {
-		this.descriptionCourse = descriptionCourse;
+		this.description_course = descriptionCourse;
 	}
 
 	public String getLieu() {
-		return lieu;
+		return lieu_course;
 	}
 
 	public void setLieu(String lieu) {
-		this.lieu = lieu;
+		this.lieu_course = lieu;
 	}
 
 	public int getNbParticipants() {
@@ -83,11 +152,11 @@ public class Course {
 	}
 
 	public String getCommentaire() {
-		return commentaire;
+		return commentaire_course;
 	}
 
 	public void setCommentaire(String commentaire) {
-		this.commentaire = commentaire;
+		this.commentaire_course = commentaire;
 	}
 
 	public float getBudget() {
@@ -138,18 +207,50 @@ public class Course {
 		this.agrement = agrement;
 	}
 
-	public boolean isPredefini() {
+	public int isPredefini() {
 		return predefini;
 	}
 
-	public void setPredefini(boolean predefini) {
+	public void setPredefini(int predefini) {
 		this.predefini = predefini;
 	}
 	
-	public void affecter(tâche t, contact c) {
+	/*
+	 * public void affecter(tache t, contact c) {
 		
 	}
+	*/
+	
+	private void writeResultSet(ResultSet resultSet) throws SQLException {
+        while (resultSet.next()) {
+            String nom = resultSet.getString("nom_course");
+         
+           
+        }
 	
 }
+	
+	 private void close() {
+	        try {
+	            if (resultSet != null) {
+	                resultSet.close();
+	            }
 
-;;;;;
+	            if (statement != null) {
+	                statement.close();
+	            }
+
+	            if (connect != null) {
+	                connect.close();
+	            }
+	        } catch (Exception e) {
+
+	        }
+	        
+	        
+	    }
+
+	 
+
+
+}
