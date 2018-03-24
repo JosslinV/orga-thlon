02097f6097ff95428176ea3184ee6b6f05d3e9b5
JosslinV <josslinvaron@gmail.com>
2018-03-24 16:04:31 +0100

    Vide la classe 'main'

--- src/modele/Main.java
+++ src/modele/Main.java
@@ -1,17 +1,9 @@
 package modele;
 
-import Database.InputDataBase.InputCourse;
 public class Main {
 	
     public static void main(String[] args) throws Exception {
-<<<<<<< HEAD
-    	InputCourse.inputNom("test", false);
-=======
-       //MySQLAccess dao = new MySQLAccess();
-        Course2 c2 = new Course2("testnom", false) ;
-        System.out.println(c2.getNom(1)) ;
-      // dao.readDataBase();
->>>>>>> refs/remotes/origin/master
+
     }
 
 }
