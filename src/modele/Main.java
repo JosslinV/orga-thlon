f634e21c8c4d9e4763989af7b74a999edd0bed8c
Josslin Varon <josslinvaron@gmail.com>
2018-04-04 15:40:11 +0200

    Nettoi la classe

--- src/modele/Main.java
+++ src/modele/Main.java
@@ -1,9 +1,14 @@
 package modele;
 
+import java.util.Date;
+
+import Database.InputDataBase.InputCourse;
+
 public class Main {
 	
     public static void main(String[] args) throws Exception {
-
+    	Course c2 = new Course("coucou",false);
+    	c2.setDateC(new Date());
+    	InputCourse.input(c2);
     }
-
 }
