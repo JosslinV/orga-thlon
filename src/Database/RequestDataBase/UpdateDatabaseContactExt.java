91220238a07150ba76a3587eb47daa1e7115004e
Maison <Maison@192.168.1.15>
2018-04-27 14:17:44 +0200

    modif update contactExt pour un seul paramètre

--- src/Database/RequestDataBase/UpdateDatabaseContactExt.java
+++ src/Database/RequestDataBase/UpdateDatabaseContactExt.java
@@ -5,6 +5,7 @@
 	import java.util.Date;
 
 	import Database.DatabaseSettings;
+import modele.contacts.ContactExterne;
 
 public class UpdateDatabaseContactExt {
 
@@ -26,20 +27,21 @@ public class UpdateDatabaseContactExt {
 
 		*/
 
-	public void UpdateDatabaseContactExt(int id_Contact, String nomSociete, String nom_c, String prenom_c, String adresse, int cp_c, String ville_c, String mail_c, String telephone_c) throws Exception {
+	public void UpdateDatabaseContactExt (ContactExterne donnees ) throws Exception {
+	//(int id_Contact, String nomSociete, String nom_c, String prenom_c, String adresse, int cp_c, String ville_c, String mail_c, String telephone_c) throws Exception {
 				
 		try {
 			Connection database = DatabaseSettings.connect();
 
-			String requete = "UPDATE orgathlon.Contact_Ext" + " SET nom_c='" + nom_c
-														+"'AND nomSociete='" + nomSociete
-														+"'AND prenom_c='" + prenom_c
-														+"'AND adresse='" + adresse
-														+"'AND cp_c=" + cp_c
-														+"AND mail_c='" + mail_c
-														+"'AND ville_c='" + ville_c
-														+"'AND telephone_c='" + telephone_c
-														+"'WHERE id_Contact = " + id_Contact ;
+			String requete = "UPDATE orgathlon.Contact_Ext" + " SET nom_c='" + donnees.getNom_c()
+														+"'AND nomSociete='" + donnees.getNomSociete()
+														+"'AND prenom_c='" + donnees.getPrenom_c()
+														+"'AND adresse='" + donnees.getAdresse()
+														+"'AND cp_c=" + donnees.getCp_c()
+														+"AND mail_c='" + donnees.getMail_c()
+														+"'AND ville_c='" + donnees.getVille_c()
+														+"'AND telephone_c='" + donnees.getTelephone_c()
+														+"'WHERE id_Contact = " + donnees.getId_personne() ;
 			Statement stmt = database.createStatement() ;
 			stmt.executeQuery(requete) ;
 			
