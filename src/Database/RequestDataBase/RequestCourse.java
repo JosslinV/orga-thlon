91afd2a19089563373ef297918aca744e085b97d
Maison <Maison@192.168.1.15>
2018-04-29 17:39:12 +0200

    ajout attributs manquants (2ème tentative après résolution conflit git)

--- src/Database/RequestDataBase/RequestCourse.java
+++ src/Database/RequestDataBase/RequestCourse.java
@@ -14,27 +14,30 @@ public class RequestCourse {
 			Connection database = DatabaseSettings.connect();
 
 			Statement state = database.createStatement();
-			ResultSet result = state.executeQuery("SELECT * FROM Course WHERE id_Course = "+ id +";");
+			ResultSet result = state.executeQuery("SELECT * FROM course WHERE id = "+ id +";");
 			
-			if(result.next()) {
-				Course course = new Course(result.getString("nom_course"), result.getBoolean("agrementFTTRi"));
-				
-				course.setFormat(result.getString("format_course"));
-				course.setDateC(result.getDate("date_course"));
-				course.setDescriptionCourse(result.getString("description_course"));			
-				course.setLieu(result.getString("lieu"));
-				course.setBudget(result.getShort("budget"));
-				course.setNbParticipants(result.getInt("nb_participants"));
-				course.setParcours(result.getString("parcours"));
-				course.setNom_ville(result.getString("nom_ville"));
-				course.setCp_ville(result.getInt("CP_ville"));
-				course.setType_epreuve(result.getString("type_epreuve"));	
-				
-				return course;
-			}
-
+			Course course = new Course(result.getString("nom_course"), result.getBoolean("agrementFTTri"));
+			course.setFormat(result.getString("format_course"));
+			course.setDateC(result.getDate("date_course"));
+			course.setDescriptionCourse(result.getString("description_course"));			
+			course.setLieu(result.getString("lieu"));
+			course.setBudget(result.getShort("budget"));
+			course.setNbParticipants(result.getInt("nb_participants"));
+			course.setParcours(result.getString("parcours"));
+			course.setNom_ville_nat(result.getString("nom_ville_nat"));
+			course.setNom_ville_cyc(result.getString("nom_ville_cyc"));
+			course.setNom_ville_cou(result.getString("nom_ville_cou"));
+			course.setNom_ville_arr(result.getString("nom_ville_arr"));
+			course.setCP_ville_nat(result.getString("CP_ville_nat"));
+			course.setCP_ville_cyc(result.getString("CP_ville_cyc"));
+			course.setCP_ville_cou(result.getString("CP_ville_cou"));
+			course.setCP_ville_arr(result.getString("CP_ville_arr"));
+			course.setDistance_nat(result.getInt("distance_nat"));
+			course.setDistance_cyc(result.getInt("distance_cyc"));
+			course.setDistance_cou(result.getInt("distance_cou"));
+			course.setType_epreuve(result.getString("type_epreuve"));		
 			
-			
+			return course;
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -42,4 +45,5 @@ public class RequestCourse {
 		}
 		return null;
 	}
+	;
 }
