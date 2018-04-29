488a35f1bd161f94a535d09acedd9a71b98e349e
Maison <Maison@192.168.1.15>
2018-04-29 17:48:39 +0200

    ajout des nouveaux attributs dans input course

--- src/Database/InputDataBase/InputCourse.java
+++ src/Database/InputDataBase/InputCourse.java
@@ -25,8 +25,18 @@ public class InputCourse {
 																+donnees.getBudget()+","
 																+donnees.getNbParticipants()+","
 																+donnees.getParcours()+","
-																+donnees.getNom_ville()+","
-																+donnees.getCp_ville()+","
+																+donnees.getDistance_nat()+","
+																+donnees.getDistance_cyc()+","
+																+donnees.getDistance_cou()+","
+																+donnees.getNom_ville_nat()+","
+																+donnees.getNom_ville_cyc()+","
+																+donnees.getNom_ville_cou()+","
+																+donnees.getNom_ville_arr()+","
+																+donnees.getCP_ville_nat()+","
+																+donnees.getCP_ville_cyc()+","
+																+donnees.getCP_ville_cou()+","
+																+donnees.getCP_ville_arr()+","
+															
 																+donnees.getType_epreuve()+");";
 			
 			Statement stmt = database.createStatement() ;
