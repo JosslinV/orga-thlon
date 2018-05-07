37ee96e9fd3f8df175f3a119c45111b4701bfa78
Gege <Gege@Gege-PC.home>
2018-05-07 16:31:52 +0200

    modif String CP + input Course avec nouvdeaux attributs, reste les dates à régler

--- src/Database/InputDataBase/InputCourse.java
+++ src/Database/InputDataBase/InputCourse.java
@@ -15,17 +15,27 @@ public class InputCourse {
 		try {
 			Connection database = DatabaseSettings.connect();
 			
-			preparedStatement = database.prepareStatement("INSERT into Orgathlon.Course values (default,?,?,?,?,?,?,?,?,?,?");
+			preparedStatement = database.prepareStatement("INSERT into Orgathlon.Course values (default,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
 			preparedStatement.setString(1, donnees.getNom());
 			preparedStatement.setString(2, donnees.getFormat());
-			preparedStatement.setDate(3, null);									//TODO date
-			preparedStatement.setString(4, donnees.getDescriptionCourse());
-			preparedStatement.setString(5, donnees.getLieu());
-			preparedStatement.setInt(6, donnees.getNbParticipants());
-			preparedStatement.setString(7, donnees.getParcours());
-			preparedStatement.setFloat(8, donnees.getBudget());
-			preparedStatement.setBoolean(9, donnees.isAgrement());
-			preparedStatement.setString(10, donnees.getType_epreuve());
+			preparedStatement.setDate(3, null);		//TODO date
+			preparedStatement.setDate(4, null);			//TODO date
+			preparedStatement.setString(5, donnees.getDescriptionCourse());
+			preparedStatement.setBoolean(6, donnees.isAgrement());
+			preparedStatement.setFloat(7, donnees.getBudget());
+			preparedStatement.setInt(8, donnees.getNbParticipants());
+			preparedStatement.setInt(9, donnees.getDistance_nat());
+			preparedStatement.setInt(10, donnees.getDistance_cyc());
+			preparedStatement.setInt(11, donnees.getDistance_cou());
+			preparedStatement.setString(12, donnees.getNom_ville_nat());
+			preparedStatement.setString(13, donnees.getNom_ville_cyc());
+			preparedStatement.setString(14, donnees.getNom_ville_cou());
+			preparedStatement.setString(15, donnees.getNom_ville_arr());
+			preparedStatement.setString(16, donnees.getCP_ville_nat());
+			preparedStatement.setString(17, donnees.getCP_ville_cyc());
+			preparedStatement.setString(18, donnees.getCP_ville_cou());
+			preparedStatement.setString(19, donnees.getCP_ville_arr());
+			preparedStatement.setString(20, donnees.getType_epreuve());
 			preparedStatement.executeUpdate();
 			
 		} catch (Exception e) {
