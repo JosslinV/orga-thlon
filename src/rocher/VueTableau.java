fbd5f506980a009806447d6d25265c4ae90f59bf
Pierre <Pierre@ROG>
2018-05-09 15:12:17 +0200

    VueTableau

--- src/rocher/VueTableau.java
+++ src/rocher/VueTableau.java
@@ -1,20 +1,36 @@
 package rocher;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
 
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
-public class VueTableau extends JPanel{
-	
-	    public VueTableau() {
-	 
-	        JTable tableau = new JTable(new ModeleStatique());
-	 
-	        this.add(new JScrollPane(tableau), BorderLayout.CENTER);
-	 
-	    }
-	 
+import modele.contacts.Contact;
 
-	}
+public class VueTableau extends JScrollPane{
+	private JTable tableau;
+
+    public VueTableau(ModeleDynamiqueTable modeleDynamiqueTable) {
+    	this.tableau =new JTable(modeleDynamiqueTable);
+        this.getViewport().add((tableau));
+    	
+ 
+       
+ //       boutons.add(new JButton(new AddAction()));
+ //     boutons.add(new JButton(new RemoveAction()));
+ 
+   
+ 
+    }
+ 
+   public JTable getJTable() {
+	   return this.tableau;
+	   
+   }
+ 
+  
+}
