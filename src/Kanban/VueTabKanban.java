d847904ae6c081cf64934b9b3e0f5ae7c3678e2d
Natacha <Natacha@192.168.1.93>
2018-05-19 16:07:51 +0200

    modifie la hauteur des lignes

--- src/Kanban/VueTabKanban.java
+++ src/Kanban/VueTabKanban.java
@@ -17,11 +17,15 @@ public class VueTabKanban extends JPanel{
 		this.tabKanban = new JTable(new TabKanbanModele());
 		this.add(this.tabKanban.getTableHeader(), BorderLayout.PAGE_START);
 		JScrollPane spKanban = new JScrollPane(this.tabKanban);
+		spKanban.setPreferredSize(new Dimension(1150,1150));
 		this.tabKanban.setPreferredScrollableViewportSize(new Dimension(1150,1150)); 
 		
 		this.add(spKanban, BorderLayout.CENTER);
 		this.tabKanban.setFillsViewportHeight(true);
 		this.tabKanban.setDefaultRenderer(JPanel.class, new TableComponent());
+		for(int i = 0; i< this.tabKanban.getRowCount(); i++) {
+			this.tabKanban.setRowHeight(i, 110);
+		}
 	}
 	
 	
