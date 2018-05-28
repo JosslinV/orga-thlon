cc721d0853c7524a88e7b514d34ff70d25765e49
Gege <Gege@Gege-PC.home>
2018-05-28 22:26:49 +0200

    création d'affectation de matériel  dans le modele et mise à jour de requestUtiliser

--- src/Database/RequestDataBase/RequestUtiliser.java
+++ src/Database/RequestDataBase/RequestUtiliser.java
@@ -1,6 +1,7 @@
 package Database.RequestDataBase;
 
 import java.sql.Connection;
+import modele.AffectationMateriel;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.LinkedList;
@@ -14,13 +15,17 @@ public class RequestUtiliser {
 	
 	private List<Tache> list;
 	
-	public  List<Tache> requestUtiliser(Materiel mat) {
+	public  int requestUtiliser(Materiel mat, Tache tache) { //il faut à la fois une tache et un mat
+		int n=0 ;
 		try {
 			Connection database = DatabaseSettings.connect();
 			Statement state = database.createStatement();
 			
-			ResultSet result = state.executeQuery("SELECT * FROM Orgathlon.Utiliser WHERE id_Materiel = "+ mat.getId_materiel() +";");
+			ResultSet result = state.executeQuery("SELECT * FROM Orgathlon.Utiliser WHERE id_Materiel = "+ mat.getId_materiel() 
+																					+"AND id_tache = "+ tache.getId_tache() + ";");
 			
+			
+			/*
 			this.list = new LinkedList<Tache>();
 			
 			while(result.next()) {
@@ -29,13 +34,25 @@ public class RequestUtiliser {
 				
 				this.list.add(tac);	
 			}
+			
 			return this.list;
+			*/
+			
+			while(result.next()) {
+				//pour s'en servir
+				//AffectationMateriel am = new AffectationMateriel() ;
+				//am.AffecterMateriel(tache, mat, DB.requestUtiliser(mat, tache)")
+				n = result.getInt("quantiteAssignee")  ;
+				
+			
+			}
+			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			DatabaseSettings.close();
 		}
-		return null;
+		return n ;
 	}
 }
